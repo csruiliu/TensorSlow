4c310a8f5c93c63a6db253b8f7c5c3beac11bf39
csruiliu <rui.liu@proton.me>
2020-01-26 01:37:13 -0600

    finish basic

---
+++ tensorslow/session.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+
+class Session():
+
+    def run(self, operation, feed_dict={}):
+        nodes_postorder = traverse_postorder(operation)
+
+        # iterate node in postorder
+        for node in nodes_postorder:
+            if type(node) == placeholder:
+                node.output = feed_dict[node]
+            elif type(node) == Variable:
+                node.output = node.value
+            else:
+                node.inputs = [input_node.output for input_node in node.input_nodes]
+                node.output = node.compute(*node.inputs)
+
+            if type(node.output) == list:
+                node.output = np.array(node.output)
+
+        return operation.output 
+
+    def traverse_postorder(operation):
+        nodes_postorder = []
+        
+        def recurse(node):
+            if isinstance(node, Operation):
+                for input_node in node.input_nodes:
+                    recurse(input_node)
+            nodes_postorder.append(node)
+
+        recurse(operation)
+
+        return nodes_postorder

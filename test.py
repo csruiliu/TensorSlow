b0ec005365d4384eead63c700aafbd635ec0909f
csruiliu <rui.liu@proton.me>
2020-02-08 15:46:54 -0600

    verify test

--- test.py
+++ test.py
@@ -1,16 +1,16 @@
-from tensorslow.graph import Operation
-from tensorslow.graph import Graph
-from tensorslow.graph import Variable
-from tensorslow.graph import placeholder
-from tensorslow.operations import *
+from graph import Graph, Variable, Placeholder
+from operations import *
+from session import Session
 
 Graph().as_default()
 
 A = Variable([[1,0],[0,-1]])
 b = Variable([1,1])
 
-x = placeholder()
+x = Placeholder()
 y = matmul(A, x)
 z = add(y, b)
 
-print(z)
+session = Session()
+output = session.run(z, {x: [1, 2]})
+print(output)
